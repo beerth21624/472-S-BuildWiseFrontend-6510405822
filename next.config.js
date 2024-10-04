86af6083ea9f24b028c483de1d5d41f95dd37ed7
Teerut26 <teerutsr@gmail.com>
2024-10-04 13:36:32 +0700

    Add Dockerfile and .dockerignore

--- next.config.js
+++ next.config.js
@@ -7,12 +7,8 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
+  swcMinify: true,
+  output: "standalone",
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
@@ -20,7 +16,7 @@ const config = {
   transpilePackages: ["geist"],
   env: {
     NEXT_PUBLIC_BASE_API: "https://general-boonkosang-be.tu4rl4.easypanel.host",
-  }
+  },
 };
 
 export default config;
