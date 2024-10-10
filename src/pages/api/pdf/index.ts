a308a7941664b019899657bbf5fb271806f2ac70
Teerut26 <teerutsr@gmail.com>
2024-10-11 01:53:48 +0700

    Refactor AppLayout to exclude certain pathnames from rendering children

--- src/pages/api/pdf/index.ts
+++ src/pages/api/pdf/index.ts
@@ -22,28 +22,24 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   await page.setViewport({ width: 1000, height: 0 });
 
-  await page.goto(`${process.env.NEXTAUTH_URL}/pdf/test`);
+  await page.goto(`${process.env.NEXTAUTH_URL}/pdf`);
 
   const pdfOption: PDFOptions = {
     printBackground: true,
     format: "A4",
+    landscape: true,
   };
 
   const pdf = await page.pdf(pdfOption);
+  const buffer = Buffer.from(pdf);
 
   await page.close();
   await browser.close();
 
-  //   set file name
   res.setHeader("Content-Disposition", `inline; filename="test.pdf"`);
   res.setHeader("Content-Type", "application/pdf");
-  res.send(pdf);
-};
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
+  res.setHeader("Content-Length", buffer.length);
+  res.send(buffer);
 };
 
 export default handler;
