fac6a628bab4cf33bd9d236c3d5e1df32ba9db5c
Teerut26 <teerutsr@gmail.com>
2025-02-21 16:04:23 +0700

    feat: enhance ContractForm with dynamic data binding and improve contract editing functionality

--- src/pages/project/[id]/document/contract/edit.tsx
+++ src/pages/project/[id]/document/contract/edit.tsx
@@ -7,12 +7,56 @@ import { type GetServerSidePropsContext, type InferGetServerSidePropsType } from
 import { useSession } from "next-auth/react";
 import ContractForm from "@/components/Document/ContractForm/ContractForm";
 import Link from "next/link";
+import useGetCompanyByUser from "@/hooks/queries/company/useGetCompanyByUser";
+import useGetContractByProject from "@/hooks/queries/contract/useGetContractByProject";
+import { parseISO } from "date-fns";
+import { ContractSchemaType } from "@/schemas/document/contract/contract.schema";
+import useEditContract from "@/hooks/mutates/contract/useEditContract";
+import { notifications } from "@mantine/notifications";
 
 export default function ContractEdit(
     props: InferGetServerSidePropsType<typeof getServerSideProps>,
 ) {
     const { data: session } = useSession();
     const getProject = useGetProject({ id: props.id ?? "" });
+    const getCompanyByUser = useGetCompanyByUser({ user_id: session?.user?.id ?? "" });
+    const getContractByProject = useGetContractByProject({ project_id: props.id ?? "" });
+    const editContract = useEditContract();
+
+    const onSave = (data: ContractSchemaType) => {
+        editContract.mutate({
+            ...data,
+            project_id: props.id ?? "",
+            format: data.format.map((value) => value.value),
+            start_date: data.start_date.toISOString(),
+            end_date: data.end_date.toISOString(),
+            periods: data.periods.map((period, index) => ({
+                ...period,
+                period_number: index + 1,
+                delivered_within: period.delivered_within ?? 0,
+                amount_period: period.amount_period ?? 0,
+                jobs: (period.jobs?.map((job) => ({
+                    job_id: job.job_id,
+                    job_amount: job.job_amount,
+                })) ?? []),
+            })),
+        }, {
+            onSuccess: () => {
+                notifications.show({
+                    title: "สําเร็จ",
+                    message: "แก้ไขสัญญาสําเร็จ",
+                    color: "green",
+                });
+            },
+            onError: (error) => {
+                notifications.show({
+                    title: "เกิดข้อผิดพลาด",
+                    message: error.message,
+                    color: "red",
+                });
+            },
+        })
+    }
 
     return (
         <div className="flex flex-col">
@@ -32,7 +76,16 @@ export default function ContractEdit(
                 </div>
             </div>
             <Divider my={"md"} />
-            <ContractForm type="edit" data={undefined} />
+            <ContractForm
+                type="edit"
+                data={getContractByProject.data ? {
+                    ...getContractByProject.data,
+                    format: getContractByProject.data.format.map((value: string) => ({ value })),
+                    start_date: parseISO(getContractByProject.data.start_date),
+                    end_date: parseISO(getContractByProject.data.end_date)
+                } : undefined}
+                onFinish={onSave}
+            />
         </div>
     )
 }
