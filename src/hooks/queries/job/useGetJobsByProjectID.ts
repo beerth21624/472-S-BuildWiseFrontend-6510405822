f79c4e5a893d110ea7dbcead413e651e6f6d550a
Teerut26 <teerutsr@gmail.com>
2025-02-25 23:08:46 +0700

    feat: enhance ContractForm to display job availability by project ID and improve error handling in contract editing

--- src/hooks/queries/job/useGetJobsByProjectID.ts
+++ src/hooks/queries/job/useGetJobsByProjectID.ts
@@ -3,10 +3,11 @@ import getJobsByProjectID, {
 } from "@/services/job/getJobsByProjectID.service";
 import { useQuery } from "@tanstack/react-query";
 
-const useGetJobsByProjectID = (props: getJobsByProjectIDProps) => {
+const useGetJobsByProjectID = (props: getJobsByProjectIDProps, key?: string) => {
   return useQuery({
-    queryKey: ["useGetJobsByProjectID"],
+    queryKey: ["useGetJobsByProjectID", key],
     queryFn: () => getJobsByProjectID(props),
+    refetchOnWindowFocus: false,
   });
 };
 
